35d71242e8032d3248d8f8565668344fa32fec40
Josefina <araya.tapia.j@gmail.com>
2020-10-23 17:41:26 -0300

    Basica 1

--- Basic.py
+++ Basic.py
@@ -1,1 +1,33 @@
 print("Hola Mundo")
+
+#Variables
+
+numero=5
+print(numero)
+cadena="Josefina"
+print(cadena)
+numero1=1
+Numero1=2
+
+print(numero1)
+print(Numero1)
+
+print(Numero1+numero1)
+
+cadena1="Hola"
+cadena2="Josefina"
+print(cadena1+" "+cadena2)
+
+print("---------------------------------------------")
+print("EJERCICIO")
+
+numero_1=5
+print(type(numero_1))
+numero_2=6.3
+print(type(numero_2))
+numero_3=7
+print(type(numero_3))
+sum=numero_1+numero_2+numero_3
+print(sum)
+print(type(sum))
+

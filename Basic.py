896efec3580ca54fc918c6f578cab4f14297557b
Josefina <araya.tapia.j@gmail.com>
2020-10-23 22:04:25 -0300

    Basic 1

--- Basic.py
+++ Basic.py
@@ -31,3 +31,70 @@ sum=numero_1+numero_2+numero_3
 print(sum)
 print(type(sum))
 
+print("---------------------------------------------")
+print("tuplas")
+##Inmutables : No se puede cambiar ni agregar datos
+
+tup1=(1,"a",True,"b",23.0)
+print(tup1[-1])
+
+print("---------------------------------------------")
+print("List")
+
+list=[1,"a",True,"b",23.0]
+print(list[-1])
+
+list[0]=100
+print(list)
+list.append("Nuevo Valor")
+print(list)
+list.append([1,2,3,4,5,6])
+print(list)
+
+
+print("---------------------------------------------")
+print("Dictionary")
+
+
+d1={"Mango":45,"platano":56}
+print(d1)
+print(d1.keys())
+print(d1.values())
+print(d1["Mango"])
+d1["Mango"]=100
+print(d1["Mango"])
+
+print("---------------------------------------------")
+print("Decisiones")
+
+a=20
+b=10
+
+if(a>b):
+    print("A es mayor que B")
+
+if(a>b) & (a>30):
+     print("A es Grande")
+elif (b<10):
+     print("B es menor que 10")    
+else:
+    print("Nada")
+
+
+
+print("---------------------------------------------")
+print("Loops") 
+
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
+            
+
+lista=["Rojo","Verde","Amarillo"]
+for i in lista:
+    print(i)
+
+
+
+
